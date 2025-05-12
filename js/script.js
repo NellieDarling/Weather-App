85ea256b550ea08b6d60032968a59fb857c521b6
NellieDarling <darlington.charnelle@gmail.com>
2025-05-12 09:25:13 -0400

    Display weather details

--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
-console.log("Hello");
+function displayWeatherInfo(res) {
+  const cityEl = document.querySelector("#city");
+  const tempEL = document.querySelector("#temperature");
+  const iconEl = document.querySelector("#icon");
+  const humidityEl = document.querySelector("#humidity");
+  const windEl = document.querySelector("#wind");
+  const conditionEl = document.querySelector("#condition");
+  cityEl.innerHTML = res.data.city;
+  tempEL.innerHTML = Math.round(res.data.temperature.current);
+  iconEl.src = res.data.condition.icon_url;
+  humidityEl.innerHTML = res.data.temperature.humidity;
+  windEl.innerHTML = res.data.wind.speed;
+  conditionEl.innerHTML = res.data.condition.description;
+  getCurrentDay();
+  getCurrentTime();
+}
+
+function getCurrentDay() {
+  const date = new Date();
+  const dayEL = document.querySelector("#day");
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  dayEL.innerHTML = daysOfWeek[date.getDay()];
+}
+
+function getCurrentTime() {
+  const date = new Date();
+  const timeEl = document.querySelector("#time");
+  const time = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  timeEl.innerHTML = time;
+}
+
+function searchCity(city) {
+  const apiKey = "9t2b4f04o4ba653b1355ab0efac7c3eb";
+  const url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(displayWeatherInfo);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  const searchInputEl = document.querySelector("#search-input");
+  searchCity(searchInputEl.value);
+}
+
+const searchFormEl = document.querySelector("#search-form");
+searchFormEl.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
